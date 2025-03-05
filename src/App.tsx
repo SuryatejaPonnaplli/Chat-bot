79944c89a9bd360cf87a3de2c3ec815fa474bbd4
SuryatejaPonnapalli <Suryateja.p@seanergydigital.com>
2025-03-05 16:50:26 +0530

    antd form commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./pages/Login/Login";
 import Chat from "./pages/Chat/Chat";
+import LoginForm from "./pages/Login/LoginForm";
 const App = () => {
   return (
     <>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Login />} />
+          <Route path="/login" element={<LoginForm />}></Route>
           <Route path="/chat" element={<Chat />} />
         </Routes>
       </BrowserRouter>
---
+++ src/pages/Login/LoginForm.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const LoginForm = () => {
+  return <div>Antd form</div>;
+};
+
+export default LoginForm;
